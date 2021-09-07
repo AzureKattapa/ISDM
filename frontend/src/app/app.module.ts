d0eb13db70cad8680cbe34a069ca852131488d1e
AzureKattapa <81165685+AzureKattapa@users.noreply.github.com>
2021-09-07 17:48:57 +0530

    1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default/default.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/bars/header/header.component';
+import { FooterComponent } from './shared/bars/footer/footer.component';
+import { SidebarComponent } from './shared/bars/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
